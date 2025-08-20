9973071c0cd6aba2a031ca3dabd668bccfd37241
TESTMECS <axwar7410@gmail.com>
2025-08-20 09:15:46 -0400

    update wezterm

--- .config/wezterm/.wezterm.lua
+++ .config/wezterm/.wezterm.lua
@@ -1,32 +1,36 @@
-local wezterm = require 'wezterm'
-local act = require 'wezterm'.action
-
+local wezterm = require("wezterm")
+local act = require("wezterm").action
 local config = wezterm.config_builder()
-
-config.initial_cols = 120
-config.initial_rows = 28
-config.font_size = 15
-config.color_scheme = "zenburned"
--- config.window_background_opacity = 0.5
--- config.text_background_opacity = 0.5
+config.color_scheme = "Wez"
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(config)
 config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 1500 }
 config.keys = {
-  { key = "-", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" })},
-  { key = "\\", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" })},
-  -- Pane navigation (h/j/k/l)
+	-- Copy mode
+	{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+	-- Split panes
+	{ key = "-", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "\\", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	-- Pane navigation (h/j/k/l)
 	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
 	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
 	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
 	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
-  -- Resize panes
+	-- Resize panes
 	{ key = "H", mods = "LEADER", action = act.AdjustPaneSize({ "Left", 5 }) },
 	{ key = "J", mods = "LEADER", action = act.AdjustPaneSize({ "Down", 5 }) },
 	{ key = "K", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
 	{ key = "L", mods = "LEADER", action = act.AdjustPaneSize({ "Right", 5 }) },
-  {
-    key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true })
-  },
-  {
+	-- Tabs
+	{
+		key = "c",
+		mods = "LEADER",
+		action = act.SpawnTab("CurrentPaneDomain"),
+	},
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = false }) },
+	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{
 		key = ",",
 		mods = "LEADER",
 		action = wezterm.action.PromptInputLine({
@@ -41,10 +45,8 @@ config.keys = {
 	{ key = "1", mods = "LEADER", action = act.ActivateTab(0) },
 	{ key = "2", mods = "LEADER", action = act.ActivateTab(1) },
 	{ key = "3", mods = "LEADER", action = act.ActivateTab(2) },
-	{ key = "4", mods = "LEADER", action = act.ActivateTab(3) },
-	{ key = "5", mods = "LEADER", action = act.ActivateTab(4) },
-  -- Session
-  {
+	{ key = "Tab", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{
 		key = "s",
 		mods = "LEADER",
 		action = wezterm.action.PromptInputLine({
@@ -68,9 +70,16 @@ config.keys = {
 	},
 }
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
+config.font = wezterm.font_with_fallback({
+	"FiraCode Nerd Font",
+	"ZedMono Nerd Font Mono",
+	"Isoveka Nerd Font",
+})
+config.default_domain = "WSL:NixOS"
+config.front_end = "WebGpu"
 return config
